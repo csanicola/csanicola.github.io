0617fccb998bac603c256a7f122c6723dfa322c5
csanicola <csanicola@users.noreply.github.com>
2025-10-22 15:55:15 +0000

    deploy: 847726b529f46c551dda98d329dd683b6aaafd04

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-dashboard",
-          title: "Dashboard",
+          title: "dashboard",
           description: "",
           section: "Navigation",
           handler: () => {
